754c32ee76798a1b4f0ee13c5ded1078c74e2922
anil4mca.kumar@gmail.com <anil4mca.kumar@gmail.com@dcc606b2-a1d7-f712-57e6-395c6bde25c2>
2013-05-02 12:23:41 +0000

    commit to src of real estate to zarvice project on 02/05/13

---
+++ trunk/RealEstate/src/com/estate/domain/Address.java
@@ -0,0 +1,58 @@
+package com.estate.domain;
+
+public class Address {
+	
+	private String dno;
+	private String street;
+	private String area;
+	private String city;
+	private String state;
+	private String pinCode;
+	private String pNo;
+	
+	public String getDno() {
+		return dno;
+	}
+	public void setDno(String dno) {
+		this.dno = dno;
+	}
+	public String getStreet() {
+		return street;
+	}
+	public void setStreet(String street) {
+		this.street = street;
+	}
+	public String getArea() {
+		return area;
+	}
+	public void setArea(String area) {
+		this.area = area;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public String getPinCode() {
+		return pinCode;
+	}
+	public void setPinCode(String pinCode) {
+		this.pinCode = pinCode;
+	}
+	public String getpNo() {
+		return pNo;
+	}
+	public void setpNo(String pNo) {
+		this.pNo = pNo;
+	}
+	
+	
+
+}
--- trunk/RealEstate/src/com/estate/domain/Portfolio.java
+++ trunk/RealEstate/src/com/estate/domain/Portfolio.java
@@ -1,5 +1,45 @@
 package com.estate.domain;
 
+import java.util.List;
+
 public class Portfolio {
+
+	private String pid;
+	private String propertyType;
+	private String propertyCategory;
+	private List<Address> addressList;
+	private String areaSize;
+	
+	public String getPid() {
+		return pid;
+	}
+	public void setPid(String pid) {
+		this.pid = pid;
+	}
+	public String getPropertyType() {
+		return propertyType;
+	}
+	public void setPropertyType(String propertyType) {
+		this.propertyType = propertyType;
+	}
+	public String getPropertyCategory() {
+		return propertyCategory;
+	}
+	public void setPropertyCategory(String propertyCategory) {
+		this.propertyCategory = propertyCategory;
+	}
+	public List<Address> getAddressList() {
+		return addressList;
+	}
+	public void setAddressList(List<Address> addressList) {
+		this.addressList = addressList;
+	}
+	public String getAreaSize() {
+		return areaSize;
+	}
+	public void setAreaSize(String areaSize) {
+		this.areaSize = areaSize;
+	}
+	
 	
 }
